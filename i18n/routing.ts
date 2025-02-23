83ddf6a40d01712e01ed9d5b2dc11a1b561c3d1d
gamessty <57580309+gamessty@users.noreply.github.com>
2025-02-23 14:04:28 +0000

    Translations completion. Added FRench

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,7 +1,7 @@
 import {defineRouting} from 'next-intl/routing';
 import {createNavigation} from 'next-intl/navigation';
 
-export const locales = ['en', 'ro'];
+export const locales = ['en', 'ro', 'fr'];
  
 export const routing = defineRouting({
   // A list of all locales that are supported
